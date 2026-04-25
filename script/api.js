cd2f685e1de8d094fda8d8f1b807a4307597d5d3
bhuvaneshchennai01-oss <bhuvaneshchennai01@gmail.com>
2026-04-25 18:17:27 +0530

    set correct backend url

--- script/api.js
+++ script/api.js
@@ -3,7 +3,7 @@
    Uses fetch(), async/await, minimal boilerplate
 ====================================================== */
 
-const API_BASE_URL = " http://127.0.0.1:8000";
+const API_BASE_URL = "https://fullstack-project-backend-seven.vercel.app";
 
 /* =========================
    HELPER FUNCTION
