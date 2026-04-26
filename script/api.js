fe7762e967ac71b5394baffc48082d65f8d0a93e
bhuvaneshchennai01-oss <bhuvaneshchennai01@gmail.com>
2026-04-26 20:25:52 +0530

    removed the local url

--- script/api.js
+++ script/api.js
@@ -1,6 +1,6 @@
 
 // const API_BASE_URL = "http://127.0.0.1:8000";
-const API_BASE_URL = "http://127.0.0.1:8000fullstack-project-backend-seven.vercel.app";
+const API_BASE_URL = "fullstack-project-backend-seven.vercel.app";
 
 
 /* =========================
